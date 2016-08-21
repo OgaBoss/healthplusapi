26ad59e0081b5c495b8fc2c00fdfa866e7907f92
damilola adebayo <OluwadamilolaAdebayo@damilolas-MacBook-Pro.local>
2016-08-21 12:10:49 +0100

    adding procfile

---
+++ database/migrations/2016_08_21_104206_add_created_by_to_hmos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatedByToHmosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hmos', function (Blueprint $table) {
+            //
+            $table->string('created_by')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hmos', function (Blueprint $table) {
+            //
+            $table->string('created_by')->default('');
+        });
+    }
+}
